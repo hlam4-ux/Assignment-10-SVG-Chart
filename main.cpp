cf109c6644419ecc917e92c796f444c54308851e
A Student <student@nowhere.edu>
2026-06-09 06:41:32 +0000

    Jun 8, 2026, 11:41 PM

--- main.cpp
+++ main.cpp
@@ -23,5 +23,9 @@ int main()
 
   ofstream outfile("chart.svg");
 
-  outfile <<
+  outfile << "<svg viewBox=\"0 -500 500 500\" width=\"500\" height=\"500\" "
+             "xmlns=\"http://www.w3.org/2000/svg\" style=\"border: 1px solid "
+             "black;\">"
+          << endl;
+  outfile << "<g transform=\"scale(1 -1)\">" << endl;
 }
