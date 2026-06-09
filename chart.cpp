ca56df3bc5bfccccd1f20039df0a9c04e698ca2e
A Student <student@nowhere.edu>
2026-06-09 04:26:27 +0000

    Jun 8, 2026, 9:26 PM

--- chart.cpp
+++ chart.cpp
@@ -5,5 +5,8 @@ double findMax(const vector<double>& data)
   double biggest = data[0];
 
   for (int i = 0; i < data.size(); i++) {
+    if (data[i] > biggest) {
+      biggest = data[i];
+    }
   }
 }
