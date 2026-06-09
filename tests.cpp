37aa30d1e5d419fa6db40f00af2ce4f61128fc06
A Student <student@nowhere.edu>
2026-06-09 06:16:05 +0000

    Jun 8, 2026, 11:16 PM

--- tests.cpp
+++ tests.cpp
@@ -15,4 +15,8 @@ TEST_CASE("scaleData")
 {
   vector<double> data = {2, 6, 4, 9, 20, 16};
   vector<double> result = scaleData(data);
+
+  CHECK(result[0] == Approx(50));
+  CHECK(result[1] == Approx(150));
+  CHECK(result[2] == Approx(100));
 }
